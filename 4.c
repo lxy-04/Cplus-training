80dec056655488948d883792eb8b24c2171146d5
李效宇04 <lixiaoyu12349@icloud.com>
2021-10-30 20:47:39 +0800

    21.10.20

--- 4.c
+++ 4.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
+#include<string.h>
+
+int mylen(const char *s){
+    int idx = 0;
+    while (*(s+idx) != '\0')
+    {
+        idx++;
+    }
+    return idx;
+
+}
 
 int main(){
-    int i =25;
-    int *p;
-    p = &i;
-    *p = 30;
-    printf("*p = %d",*p);
-    printf("p = %p",p);
-    printf("i = %d",i);
+    char *s = "hello";
+    printf("length is %d\n", mylen(s));
+    
     return 0;
 }
