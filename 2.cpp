e683952078ab1120c30254d1ee09229b9eda50d4
李效宇04 <lixiaoyu12349@icloud.com>
2021-11-19 19:00:59 +0800

    11.19-2

--- 2.cpp
+++ 2.cpp
@@ -4,25 +4,29 @@
 
 using namespace std;
 
+struct inflatable
+{
+    int price;
+};
+
+char *getname(void){
+
+    char tmp[80];
+    cout << "enter tmp" << endl;
+    cin >> tmp;
+    char *pn = new char[strlen(tmp) + 1];
+    strcpy(pn, tmp);
+    return pn;
+
+}
+
 int main(void){
 
-    /*
-    double *p3 = new double [3];
-    p3[0] = 0.1;
-    p3[1] = 0.2;
-    p3[2] = 0.3;
-    cout << "p3[1] = " << p3[1] << endl;
-    */
-    char bird[10];
-    char* bird2 = NULL;
-    cin >> bird;
-    cout << "bird is " << bird << endl;
-
-    bird2 = new char[strlen(bird) + 1];
-    strcpy(bird2, bird);
-    cout << "bird2 is " << bird2 << endl;
-    delete []bird2;
-    
-
+    inflatable s1, s2, s3;
+    inflatable *p = &s1;
+    inflatable *arp[3] = {&s1, &s2, &s3};
+    auto ppb = arp;
+    (*(ppb+2)) -> price = 100;
+    cout << "s3:" << s3.price << endl;
     return 0;
 }
