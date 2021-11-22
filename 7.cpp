4f9cafbd1d552b898fc8efbef739d9faba6a1781
李效宇04 <lixiaoyu12349@icloud.com>
2021-11-22 20:14:31 +0800

    11.22

--- 7.cpp
+++ 7.cpp
@@ -7,31 +7,12 @@ using namespace std;
 
 int main(void){
 
-    // cout << "Enter sec " << endl;
-    // float secs;
-    // int per_time = 0;
-    // cin >> secs;
-    // clock_t delay = secs * CLOCKS_PER_SEC;
-    // clock_t st_time = clock();
-    // cout << "st_time is " << st_time << endl;
-    // while (clock() - st_time < delay){
-    //     if ( ((clock() - st_time) % CLOCKS_PER_SEC) == 0)
-    //     {
-    //         cout << clock() << endl;
-    //         per_time++;
-    //     }
-        
-    // }
+    char *s[2] = {
+        "hello",
+        "world"
+    };
 
-    // int i = 0;
-    // while (i <= 5)
-    // {
-        
-    //     cout << "i: " << i << endl;
-    //     i++;
-    // }
-    
-    // cout << "done i: " << i << endl;
+    cout << "done" << endl;
     
     return 0;
 }
