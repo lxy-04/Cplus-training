09b2c90dcd501ac05231a9c231dbeb9009a9ca4c
李效宇04 <lixiaoyu12349@icloud.com>
2021-11-23 17:27:19 +0800

    11.23.2

---
+++ 9.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+enum color {red, yellow};
+
+using namespace std;
+
+int main(void){
+
+    // char ch;
+    // int count = 0;
+    // cin.get(ch);
+
+    // while (ch != '@')
+    // {
+    //     cout << ch << endl;
+    //     ++count;
+    //     cin.get(ch);
+    // }
+
+    // cout << "count is " << count << endl;
+    color your_color;
+    your_color = yellow;
+    if (your_color == yellow)
+    {
+        cout << "your color is " << your_color << endl;
+    }
+    
+    
+    
+
+    return 0;
+}
