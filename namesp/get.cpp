de4a5b0de8af13a4dfd20ac60d63d6e62d1b62d6
李效宇04 <lixiaoyu12349@icloud.com>
2021-12-05 12:56:51 +0800

    12.5.1

--- namesp/get.cpp
+++ namesp/get.cpp
@@ -9,29 +9,20 @@ int main(void)
 {
     golf golfs[SIZE] ;
     int count = 0;
-    int full_enter = 0;
-    // setgolf(golfs[0], "lixiaoyu", 1000);
-    // showgolf(golfs[0]);
-    while ( full_enter < SIZE )
+
+    while ( count < SIZE )
     {
         setgolf(golfs[count]);
-        showgolf(golfs[count]);
         if (golfs[count].fullname[count] == '\0')
-        {
             break;
-        }
-        else{
-            ++full_enter;
-        }
         ++count;
-        count = count % SIZE;
     }
     
     for (int i = 0; i < count; i++)
     {
         if (setgolf(golfs[i]))
         {
-            cout << "origin golf is: " << endl;
+            cout << "\norigin golf is: " << endl;
             showgolf(golfs[i]);
             handicap(golfs[i], i + 200);
             cout << "After reseting: " << endl;
