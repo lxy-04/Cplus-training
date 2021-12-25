a937e83a6620178e51b43762521e0b32b9496595
李效宇-04 <lixiaoyu12349@icloud.com>
2021-12-25 12:39:14 +0800

    12.25.1

--- port/1.cpp
+++ port/1.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main(void)
 {
-
+    Port port1("ABC", "SWEET", 200);
+    cout << port1 << endl;
+    cout << "-------------" << endl;
+    Vport vp("EFG", "vintage", 300, "old jack", 40);
+    cout << vp << endl;
+    Vport vp2(vp);
+    cout << "-------------" << endl;
+    cout << vp2 << endl;
+    Port *p_port = &vp;
+    p_port -> Show();
     return 0;
 }
